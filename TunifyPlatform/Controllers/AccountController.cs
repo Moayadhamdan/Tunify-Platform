78e8e798ea120c99a760aa9c7eb5296b0e0dc425
Moayadhamdan <hamadanjo@gmail.com>
2024-08-24 22:52:36 +0300

    Tunify Platform - Identity Implementation done

---
+++ TunifyPlatform/Controllers/AccountController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TunifyPlatform.Models.DTO;
+using TunifyPlatform.Repositories.Interfaces;
+
+namespace TunifyPlatform.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private readonly IAccountRepository _accountService;
+
+        public AccountController(IAccountRepository accountService)
+        {
+            _accountService = accountService;
+        }
+        // register
+        [HttpPost("Register")]
+        public async Task<ActionResult<AccountDto>> Register(RegisterDto registerDto)
+        {
+            var user = await _accountService.RegisterUser(registerDto);
+            return Ok(user);
+        }
+
+
+        // login 
+        [HttpPost("Login")]
+        public async Task<ActionResult<AccountDto>> Login(LoginDto loginDto)
+        {
+            var user = await _accountService.UserAuthentication(loginDto.UserName, loginDto.Password);
+
+            if (user == null)
+            {
+                return Unauthorized();
+            }
+
+            return user;
+        }
+
+        // logout
+        [HttpPost("Logout")]
+        public async Task<ActionResult<AccountDto>> LogOut(string username)
+        {
+            var newLogout = await _accountService.LogOut(username);
+            return newLogout;
+        }
+    }
+}
---
+++ TunifyPlatform/Repositories/Interfaces/IAccountRepository.cs
@@ -0,0 +1,16 @@
+﻿using TunifyPlatform.Models.DTO;
+
+namespace TunifyPlatform.Repositories.Interfaces
+{
+    public interface IAccountRepository
+    {
+        // Add register
+        public Task<AccountDto> RegisterUser(RegisterDto registerDto);
+
+        // Add login
+        public Task<AccountDto> UserAuthentication(string username, string password);
+
+        // Add logout
+        public Task<AccountDto> LogOut(string username);
+    }
+}
